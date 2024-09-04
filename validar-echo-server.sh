7836036db1a1ea323a6d36651cab43ca8139aae8
franciscoduc4 <fduca@fi.uba.ar>
2024-09-04 20:33:21 -0300

    fixing name

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,5 +1,5 @@
 # #!/bin/bash
-NETWORK_NAME="tp0-base_testing_net"
+NETWORK_NAME="tp0_testing_net"
 
 if ! docker network inspect $NETWORK_NAME >/dev/null 2>&1; then
     echo "Network $NETWORK_NAME not found"
