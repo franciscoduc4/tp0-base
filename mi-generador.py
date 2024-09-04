e9ae8efa6c9e5a69eb101cabb3b954efa0872f6b
franciscoduc4 <fduca@fi.uba.ar>
2024-09-04 16:03:24 -0300

    fix ej1

--- mi-generador.py
+++ mi-generador.py
@@ -1,47 +1,61 @@
 import sys
 
-def generate_docker_compose(output_file, num_clients):
-    with open(output_file, 'w') as f:
-        f.write("version: '3.8'\n")
-        f.write("services:\n")
-        f.write("  server:\n")
-        f.write("    container_name: server\n")
-        f.write("    image: server:latest\n")
-        f.write("    entrypoint: python3 /main.py\n")
-        f.write("    environment:\n")
-        f.write("      - PYTHONUNBUFFERED=1\n")
-        f.write("      - LOGGING_LEVEL=DEBUG\n")
-        f.write("    networks:\n")
-        f.write("      - testing_net\n")
-        
-        for i in range(1, num_clients + 1):
-            f.write(f"\n  client{i}:\n")
-            f.write(f"    container_name: client{i}\n")
-            f.write("    image: client:latest\n")
-            f.write("    entrypoint: /client\n")
-            f.write("    environment:\n")
-            f.write(f"      - CLI_ID={i}\n")
-            f.write("      - CLI_LOG_LEVEL=DEBUG\n")
-            f.write("    networks:\n")
-            f.write("      - testing_net\n")
-            f.write("    depends_on:\n")
-            f.write("      - server\n")
-        
-        f.write("\nnetworks:\n")
-        f.write("  testing_net:\n")
-        f.write("    ipam:\n")
-        f.write("      driver: default\n")
-        f.write("      config:\n")
-        f.write("        - subnet: 172.25.125.0/24\n")
+def generateCompose(fileName, clientsNumber):
+    content = """
+services:
+"""
+
+    server = """  server:
+    container_name: server
+    image: server:latest
+    entrypoint: python3 /main.py
+    environment:
+      - PYTHONUNBUFFERED=1
+      - LOGGING_LEVEL=DEBUG
+    networks:
+      - testing_net
+
+"""
     
-    print(f"Archivo Docker Compose generado en {output_file} con {num_clients} clientes.")
+    content += server
+
+
+    for i in range(1, int(clientsNumber) + 1):
+        client = f"""  client{i}:
+    container_name: client{i}
+    image: client:latest
+    entrypoint: /client
+    environment:
+      - CLI_ID={i}
+      - CLI_LOG_LEVEL=DEBUG
+    networks:
+      - testing_net
+    depends_on:
+      - server
+
+"""
+        content += client
+
+    networks =  """networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24
+"""
+
+    content += networks
+
+    with open(fileName, 'w') as file:
+        file.write(content)
+
+    print(f"OK")
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Uso: python3 mi-generador.py <nombre_del_archivo_de_salida> <cantidad_de_clientes>")
         sys.exit(1)
-    
-    output_file = sys.argv[1]
-    num_clients = int(sys.argv[2])
-    
-    generate_docker_compose(output_file, num_clients)
+
+    fileName = sys.argv[1]
+    clientsNumber = sys.argv[2]
+
+    generateCompose(fileName, clientsNumber)
