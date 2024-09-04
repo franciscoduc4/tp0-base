b3db0e150a19a9ff4b78d7c155d474d809d77b37
franciscoduc4 <fduca@fi.uba.ar>
2024-09-03 23:11:48 -0300

    test

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,46 +1,13 @@
-#!/bin/bash
-
-# Defino las variables
+# #!/bin/bash
 NETWORK_NAME="echo_server_network"
-SERVER_CONTAINER_NAME="echo_server"
-MESSAGE="Hello, EchoServer!"
+SERVER_HOST="server"
+SERVER_PORT="12345"
+MESSAGE="Hello Server"
 
-# # Elimina la red docker si ya existe
-# docker network rm $NETWORK_NAME 2>/dev/null || true
+RESPONSE=$(docker run --rm --network ${NETWORK_NAME} busybox sh -c "echo '${MESSAGE}' | nc ${SERVER_HOST} ${SERVER_PORT}")
 
-# # Elimina el contenedor del servidor si ya existe
-# docker rm -f $SERVER_CONTAINER_NAME 2>/dev/null || true
-
-# # Elimina el contenedor del cliente si ya existe
-# docker rm -f $CLIENT_CONTAINER_NAME 2>/dev/null || true
-
-# # Crea una nueva red docker
-# docker network create $NETWORK_NAME
-
-# # Corre el contenedor del echoserver
-# docker run -d --name $SERVER_CONTAINER_NAME --network $NETWORK_NAME $SERVER_IMAGE
-
-# # Verifica el estado del contenedor del servidor
-# if [ "$(docker ps -q -f name=$SERVER_CONTAINER_NAME)" ]; then
-#   echo "Servidor iniciado correctamente."
-# else
-#   echo "Error: El contenedor del servidor no se está ejecutando."
-#   exit 1
-# fi
-
-# Corre un contenedor temporal con netcat y verificar respuesta
-RESPONSE=$ docker run --rm --network $NETWORK_NAME busybox sh -c "
-  echo '$MESSAGE' | nc $SERVER_CONTAINER_NAME 12345" 
-
-# RESPONSE=$(cat response.txt)
-
-if [ "$MESSAGE" == "$RESPONSE" ]; then
-  echo "action: test_echo_server | result: success"
+if [ "$RESPONSE" = "$MESSAGE" ]; then
+    echo "action: test_echo_server | result: success"
 else
-  echo "action: test_echo_server | result: fail"
+    echo "action: test_echo_server | result: fail"
 fi
-
-# # Limpio los recursos
-# docker rm -f $SERVER_CONTAINER_NAME
-# docker network rm $NETWORK_NAME
-# rm response.txt
